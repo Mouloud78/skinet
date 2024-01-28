802ca20818d7adb9fa68c1e57601613b7192666d
mouloud78 <mouloudouldali78@gmail.com>
2024-01-27 21:41:05 -0500

    add product entity

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbf30776dfe5130afacda67e88e265b712ce1973")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4befbdc9e8781b184180b4ca1ce73745066f8f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
