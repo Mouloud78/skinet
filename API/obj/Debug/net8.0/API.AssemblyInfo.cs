af1e8d5146c74f226cac43a99829107a82d19f26
mouloud78 <mouloudouldali78@gmail.com>
2024-02-06 14:45:25 -0500

    Create table with update

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36aa607d694d0ed6a8d0055e227c177ff28eef0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c89f46bdae02d1959745744210de7bb131931a0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
