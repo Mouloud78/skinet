a99df01b968d57909cef71235186ae85dcc1c18f
mouloud78 <mouloudouldali78@gmail.com>
2024-10-21 10:30:03 -0400

    sorting products

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -1,3 +1,4 @@
+using System.Security.Cryptography;
 using Core.Entities;
 using Core.Interfaces;
 using Microsoft.EntityFrameworkCore;
@@ -16,14 +17,42 @@ public class ProductRepository(StoreContext context) : IProductRepository
         context.Productes.Remove(product);
     }
 
+    public async Task<IReadOnlyList<string>> GetBrandsAsync()
+    {
+        return await context.Productes.Select(x => x.Brand)
+        .Distinct()
+        .ToListAsync();
+    }
+
     public async Task<Product> GetProductByIdAsync(int id)
     {
         return await context.Productes.FindAsync(id);
     }
 
-    public async Task<IReadOnlyList<Product>> GetProductsAsync()
+    public async Task<IReadOnlyList<Product>> GetProductsAsync(string brand,
+                                                               string type,
+                                                               string sort)
     {
-        return await context.Productes.ToListAsync();
+        var query = context.Productes.AsQueryable();
+        if (!string.IsNullOrWhiteSpace(brand))
+            query = query.Where(x => x.Brand == brand);
+        if (!string.IsNullOrWhiteSpace(type))
+            query = query.Where(x => x.Type == type);
+
+        query = sort switch
+        {
+            "priceAsc" => query.OrderBy(x => x.Price),
+            "priceDesc" => query.OrderByDescending(x => x.Price),
+            _ => query.OrderBy(x => x.Name)
+        };
+        return await query.ToListAsync();
+    }
+
+    public async Task<IReadOnlyList<string>> GetTypesAsync()
+    {
+        return await context.Productes.Select(x => x.Type)
+        .Distinct()
+        .ToListAsync();
     }
 
     public bool ProducExists(int id)
