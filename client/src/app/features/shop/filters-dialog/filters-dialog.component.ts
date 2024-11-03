ab5cec71e3dc828e3d8cede5420335fd77fd8e50
mouloud78 <mouloudouldali78@gmail.com>
2024-11-03 10:36:24 -0500

    Hooking  up the filters to the service

--- client/src/app/features/shop/filters-dialog/filters-dialog.component.ts
+++ client/src/app/features/shop/filters-dialog/filters-dialog.component.ts
@@ -4,11 +4,18 @@ import { MatDivider } from '@angular/material/divider';
 import { MatListOption, MatSelectionList } from '@angular/material/list';
 import { MatButton } from '@angular/material/button';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-filters-dialog',
   standalone: true,
-  imports: [MatDivider, MatSelectionList, MatListOption, MatButton],
+  imports: [
+    MatDivider,
+    MatSelectionList,
+    MatListOption,
+    MatButton,
+    FormsModule,
+  ],
   templateUrl: './filters-dialog.component.html',
   styleUrl: './filters-dialog.component.scss',
 })
